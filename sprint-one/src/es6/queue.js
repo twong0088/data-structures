91de9370511b96142f8122d173eed4a7f509ebf9
Terrence Wong <terrencewong223@gmail.com>
2020-10-07 15:13:55 -0700

    completed es6

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+    this.start = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+  dequeue() {
+    if (this.counter > this.start) {
+      var memory = this.storage[this.start];
+      delete this.storage[this.start];
+      this.start++;
+      return memory;
+    }
+  }
+  size() {
+    return this.counter - this.start;
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.counter = 0;
+  }
+  push(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  }
+  pop() {
+    if (this.counter > 0) {
+      this.counter--;
+      var memory = this.storage[this.counter];
+      delete this.storage[this.counter];
+      return memory;
+    }
+  }
+  size() {
+    return this.counter;
   }
 
 }
