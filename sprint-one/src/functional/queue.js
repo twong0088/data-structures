b547333ebcf2d2124a216109c8f8df97a4c19e01
Terrence Wong <terrencewong223@gmail.com>
2020-10-07 12:09:28 -0700

    Passed all test cases for functional

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,29 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var counter = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[counter] = value;
+
+    counter++;
   };
 
   someInstance.dequeue = function() {
+    if (counter > start) {
+      var memory = storage[start];
+      delete storage[start];
+      start++;
+
+      return memory;
+    }
   };
 
   someInstance.size = function() {
+    return counter - start;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var counter = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    if (counter > 0) {
+      counter--;
+      var memory = storage[counter];
+      delete storage[counter];
+      return memory;
+    }
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
