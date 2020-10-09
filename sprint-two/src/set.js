070443e779d83db442ab424cd8c3cfa88a840184
Terrence Wong <terrencewong223@gmail.com>
2020-10-08 17:16:58 -0700

    Passed all tests for set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  for (key in this._storage) {
+    if (this._storage[key] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  delete this._storage[item];
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ O(n)
  */
